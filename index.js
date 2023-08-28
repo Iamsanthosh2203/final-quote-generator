8bdc7616167855f3a17ab48912209f43b7257e01
Santhosh Kumar <iamsanthosh2203@gmail.com>
2023-08-28 17:50:20 +0530

    Fixed-Input

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const resetBtn = document.querySelector("#reload");
 
 // 4. Configure the AI by setting a temperature from 0 to 1
 // The higher temperature, the more random & experimental output
-let temperature = 1;
+let temperature = 0;
 
 // Optional: delete "avatar.jpg" and add a photo of yourself
 // (remember to use "avatar.jpg" as the name of your photo)
@@ -31,17 +31,22 @@ btn.addEventListener("click", () => {
   let favoriteActivity = document.getElementById("myHobbie").value;
   let favoritePlace = document.getElementById("myPlace").value;
   console.log(name, favoriteActivity, favoritePlace);
-  generateTextAndImage(name, favoriteActivity, favoritePlace, temperature);
-  nameDOM.classList.add("none");
-  favoriteActivityDOM.classList.add("none");
-  favoritePlaceDOM.classList.add("none");
 
-  quoteDOM.classList.remove("none");
-  namespanDOM.classList.remove("none");
+  if (!name || !favoriteActivity || !favoritePlace) {
+    alert("Please fill all the forms!!!");
+  } else {
+    generateTextAndImage(name, favoriteActivity, favoritePlace, temperature);
+    nameDOM.classList.add("none");
+    favoriteActivityDOM.classList.add("none");
+    favoritePlaceDOM.classList.add("none");
 
-  btn.classList.add("none");
+    quoteDOM.classList.remove("none");
+    namespanDOM.classList.remove("none");
 
-  resetBtn.classList.remove("none");
+    btn.classList.add("none");
+
+    resetBtn.classList.remove("none");
+  }
 });
 
 resetBtn.addEventListener("click", () => {
